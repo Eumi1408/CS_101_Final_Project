41f3c85e3d1f1338ac1d654ee3c42c141611b802
Eumi <75530429+Eumi1408@users.noreply.github.com>
2022-04-01 15:23:13 -0400

    Magic trick is functional. Refactoring needed

--- number_guessing_magic_trick.py
+++ number_guessing_magic_trick.py
@@ -2,25 +2,15 @@
 
 ## Global Variables
 final_number = 0
-enter_continue = input("Press Enter to continue...")
-input_1 = input("""Choose a number between 1 and 10, let's call it 'a' and add it to the number you picked. 
-Type 'a' here and press enter.""")
-input_2 = input("""Choose a number between 1 and 10, let's call it 'b' and subtract it from the number you picked. 
-Type 'b' here and press enter.""")
-input_3 = input("""Choose a number between 1 and 10, let's call it 'c' and subtract it from the number you picked. 
-Type 'c' here and press enter.""")
-input_4 = input("""Choose a number between 1 and 10, let's call it 'd' and add it to the number you picked. 
-Type 'd' here and press enter.""")
 subtract_original_number = """Take the number you have calculated up to this point. 
 Then, subtract your original number 'x' from this number. The result is your final number.
 (like this: number - x = final number)."""
-last_step = input("Press Enter to reveal my guess for your final number")
 
 ## Answer Function
 def final_answer(answer):
-    print(input("Is this correct? Type yes or no, then press enter."))
     if answer == "yes":
         print("""MUHAHA. I dare you to play again! Press Ctrl+L to start over.
+        Then, type 'python number_guessing_magic_game.py' and press Enter.
         .-._                                                   _,-,
   `._`-._                                           _,-'_,'
      `._ `-._                                   _,-' _,'
@@ -35,20 +25,26 @@ Deelkar        #   `"._(_).       .(_)_."'   # """)
 
 ## Program
 print("Welcome to this Python 3 Magic Session.")
-print(input("Press Enter to begin..."))
+input("Press Enter to begin...")
 print("""Please pick a number between 1 and 20. Let's call it 'x'.
 Keep it to yourself. Do NOT enter it at any point during the session. 
 Get ready to perform a series of simple calculations over the next few steps.""")
-print(enter_continue)
-print(input_1)
-final_number += input_1
-print(input_2)
-final_number -= input_2
-print(input_3)
-final_number += input_3
-print(input_4)
-final_number += input_4
+input("Press Enter to continue...")
+input_1 = input("""Choose a number between 1 and 10, let's call it 'a' and add it to the number you picked. 
+Type 'a' here and press enter. """)
+final_number += int(input_1)
+input_2 = input("""Choose a number between 1 and 10, let's call it 'b' and subtract it from the number you picked. 
+Type 'b' here and press enter. """)
+final_number -= int(input_2)
+input_3 = input("""Choose a number between 1 and 10, let's call it 'c' and add it to the number you picked. 
+Type 'c' here and press enter. """)
+final_number += int(input_3)
+input_4 = input("""Choose a number between 1 and 10, let's call it 'd' and subtract it from the number you picked. 
+Type 'd' here and press enter. """)
+final_number -= int(input_4)
 print(subtract_original_number)
-print(last_step)
+input("Press Enter to continue...")
+input("Press Enter to reveal my guess for your final number.")
 print(final_number)
-print(final_answer)
+yes_no = input("Is this correct? Type yes or no, then press enter. ")
+print(final_answer(yes_no))
