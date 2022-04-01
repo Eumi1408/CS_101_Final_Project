1205665327d20dc38ac7b2b43dd0d54a2454498d
Eumi <75530429+Eumi1408@users.noreply.github.com>
2022-04-01 09:49:25 -0400

    First commit

---
+++ #Number_Guessing_Magic_Trick.py
@@ -0,0 +1,11 @@
+#Number_Guessing_Magic_Trick
+
+final_number = 0
+enter_continue = input("Press Enter to continue...")
+print("Welcome to this Python 3 Magic Session.")
+print(input("Press Enter to begin..."))
+print("""Please pick a number between 1 and 10.
+Do NOT enter it at any point during the session and do not write it down. 
+If you need to write it down, you can cover your webcam to make sure I don't cheat!
+Get ready to perform a series of simple calculations in the next few steps.""")
+print(enter_continue)
