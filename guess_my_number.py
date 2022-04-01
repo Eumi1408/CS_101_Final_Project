f6f2dffe28ac665bd2b1e8dc67c52f6b77ff1567
Eumi <75530429+Eumi1408@users.noreply.github.com>
2022-04-01 17:22:59 -0400

    Debugged instructions

--- guess_my_number.py
+++ guess_my_number.py
@@ -1,4 +1,4 @@
-## Number Guessing Magic Trick
+## Guess My Number Magic Trick
 
 ## Global Variables
 final_number = 0
@@ -29,16 +29,16 @@ print("""Please pick a number between 1 and 20. Let's call it 'x'.
 Keep it to yourself. Do NOT enter it at any point during the session. 
 Get ready to perform a series of simple calculations over the next few steps.""")
 input("Press Enter to continue...")
-input_1 = input("""Choose a number between 1 and 10, let's call it 'a'. Add it to the number you picked. 
+input_1 = input("""Choose a number between 1 and 10, let's call it 'a'. Add it to the number 'x' you picked. 
 Type 'a' here and press enter. """)
 final_number += int(input_1)
-input_2 = input("""Choose a number between 1 and 10, let's call it 'b'. Subtract it from the number you picked. 
+input_2 = input("""Choose a number between 1 and 10, let's call it 'b'. Subtract it from the new number you just calculated. 
 Type 'b' here and press enter. """)
 final_number -= int(input_2)
-input_3 = input("""Choose a number between 1 and 10, let's call it 'c'. Add it to the number you picked. 
+input_3 = input("""Choose a number between 1 and 10, let's call it 'c'. Add it to the new number you just calculated. 
 Type 'c' here and press enter. """)
 final_number += int(input_3)
-input_4 = input("""Choose a number between 1 and 10, let's call it 'd'. Subtract it from the number you picked. 
+input_4 = input("""Choose a number between 1 and 10, let's call it 'd'. Subtract it from the new number you just calculated. 
 Type 'd' here and press enter. """)
 final_number -= int(input_4)
 print(subtract_original_number)
