bcc59a0e15cb576d2a2a02ca10fa96f9ca548166
Eumi <75530429+Eumi1408@users.noreply.github.com>
2022-04-01 15:35:15 -0400

    Final testing completed

--- number_guessing_magic_trick.py
+++ number_guessing_magic_trick.py
@@ -9,8 +9,7 @@ Then, subtract your original number 'x' from this number. The result is your fin
 ## Answer Function
 def final_answer(answer):
     if answer == "yes":
-        print("""MUHAHA. I dare you to play again! Press Ctrl+L to start over.
-        Then, type 'python number_guessing_magic_game.py' and press Enter.
+        return """MUHAHA. I dare you to play again! Press Ctrl+L to clear your terminal. Then, type 'python number_guessing_magic_game.py' and press Enter.
         .-._                                                   _,-,
   `._`-._                                           _,-'_,'
      `._ `-._                                   _,-' _,'
@@ -19,9 +18,9 @@ def final_answer(answer):
               `._/)  ._               _.  (\_,'
                )*'     **.__     __.**     '*( 
                #  .==..__  ""   ""  __..==,  # 
-Deelkar        #   `"._(_).       .(_)_."'   # """)
+Deelkar        #   `"._(_).       .(_)_."'   # """
     if answer == "no":
-        print("Oops, something went wrong! Please play again and use a calculator. Press Ctrl+L to start over.")
+        return """Oops, something went wrong! Please play again and use a calculator. Press Ctrl+L to clear your terminal. Then, type 'python number_guessing_magic_game.py' and press Enter."""
 
 ## Program
 print("Welcome to this Python 3 Magic Session.")
@@ -30,16 +29,16 @@ print("""Please pick a number between 1 and 20. Let's call it 'x'.
 Keep it to yourself. Do NOT enter it at any point during the session. 
 Get ready to perform a series of simple calculations over the next few steps.""")
 input("Press Enter to continue...")
-input_1 = input("""Choose a number between 1 and 10, let's call it 'a' and add it to the number you picked. 
+input_1 = input("""Choose a number between 1 and 10, let's call it 'a'. Add it to the number you picked. 
 Type 'a' here and press enter. """)
 final_number += int(input_1)
-input_2 = input("""Choose a number between 1 and 10, let's call it 'b' and subtract it from the number you picked. 
+input_2 = input("""Choose a number between 1 and 10, let's call it 'b'. Subtract it from the number you picked. 
 Type 'b' here and press enter. """)
 final_number -= int(input_2)
-input_3 = input("""Choose a number between 1 and 10, let's call it 'c' and add it to the number you picked. 
+input_3 = input("""Choose a number between 1 and 10, let's call it 'c'. Add it to the number you picked. 
 Type 'c' here and press enter. """)
 final_number += int(input_3)
-input_4 = input("""Choose a number between 1 and 10, let's call it 'd' and subtract it from the number you picked. 
+input_4 = input("""Choose a number between 1 and 10, let's call it 'd'. Subtract it from the number you picked. 
 Type 'd' here and press enter. """)
 final_number -= int(input_4)
 print(subtract_original_number)
